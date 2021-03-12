37e1f69da57f8b79eb4bf1f35e67e90313d2095f
Przemek Rura <przemekrura95@gmail.comgit config --global user.email przemekrura95@gmail.com>
2021-03-12 10:55:20 +0100

    add stats for teams

--- src/components/description/Description.js
+++ src/components/description/Description.js
@@ -5,7 +5,16 @@ const Description = () => {
     return(
         <>
             <section className="desc-wrapper">
-                <h2>bonk</h2>
+                <h2>TeamA</h2>
+                <p>Attack: q</p>
+                <p>Block: w</p>
+                <p>Serve: e</p>
+                <p>Opponent error: r</p>
+                <h2>TeamB</h2>
+                <p>Attack: y</p>
+                <p>Block: u</p>
+                <p>Serve: i</p>
+                <p>Opponent error: o</p>
             </section>
         </>
     )
--- src/components/scoreboard/Scoreboard.js
+++ src/components/scoreboard/Scoreboard.js
@@ -1,19 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './../../css/Scoreboard.css';
 
 const Scoreboard = () => {
 
-const [points, setPoints] = useState({teamA: 10, teamB: 10});
+const [points, setPoints] = useState({teamA: 0, teamB: 0});
 const [sets, setSets] = useState({teamA: 0, teamB: 0 });
 
+const [teamA, setTeamA] = useState({attack: 0, block: 0, serve:0, error: 0});
+const [teamB, setTeamB] = useState({attack: 0, block: 0, serve:0, error: 0});
 
-const handlePoints = (e) =>{
-    if (e.target.id==="teamA"){
-        setPoints({...points, teamA: points.teamA + 1})
-    } else if (e.target.id==="teamB"){
-        setPoints({...points, teamB: points.teamB + 1})
-    } 
+function useKey(key, callback) {
+    const callbackRef = useRef(callback);
+    useEffect(() => {
+        callbackRef.current = callback;
+    });
+
+
+    useEffect(() => {
+        function handle(keyEvent){
+            if(keyEvent.key === key) {
+                callbackRef.current(keyEvent)
+            }
+        }
+        document.addEventListener("keyup", handle)
+        return () => {
+            document.removeEventListener("keyup", handle)
+        };
+    }, [key]);
 }
+
 
 const setCounter = () => {
     if (points.teamA >= 21 && points.teamA >= points.teamB + 2) {
@@ -32,11 +47,57 @@ const setCounter = () => {
     }
 }
 
-
 useEffect(() => {
     setCounter()
-
 });
+
+const teamAAttack = () => {
+    setTeamA({...teamA, attack: teamA.attack + 1})
+    setPoints({...points, teamA: points.teamA + 1})
+}
+const teamABlock = () => {
+    setTeamA({...teamA, block: teamA.block + 1})
+    setPoints({...points, teamA: points.teamA + 1})
+}
+const teamAServe = () => {
+    setTeamA({...teamA, serve: teamA.serve + 1})
+    setPoints({...points, teamA: points.teamA + 1})
+}
+const teamAError = () => {
+    setTeamA({...teamA, error: teamA.error + 1})
+    setPoints({...points, teamA: points.teamA + 1})
+}
+
+const teamBAttack = () => {
+    setTeamB({...teamB, attack: teamB.attack + 1})
+    setPoints({...points, teamB: points.teamB + 1})
+}
+const teamBBlock = () => {
+    setTeamB({...teamB, block: teamB.block + 1})
+    setPoints({...points, teamB: points.teamB + 1})
+}
+const teamBServe = () => {
+    setTeamB({...teamB, serve: teamB.serve + 1})
+    setPoints({...points, teamB: points.teamB + 1})
+}
+const teamBError = () => {
+    setTeamB({...teamB, error: teamB.error + 1})
+    setPoints({...points, teamB: points.teamB + 1})
+}
+
+
+
+useKey('q', teamAAttack);
+useKey('w', teamABlock);
+useKey('e', teamAServe);
+useKey('r', teamAError);
+
+useKey('y', teamBAttack);
+useKey('u', teamBBlock);
+useKey('i', teamBServe);
+useKey('o', teamBError);
+
+
 
 
 
@@ -44,11 +105,27 @@ useEffect(() => {
             <>
                 <section className="score-wrapper">
                     <div className="result-box">
-                        <h3>TeamA points:{points.teamA}, sets:{sets.teamA}</h3>
-                        <h3>TeamB points:{points.teamB}, sets:{sets.teamB}</h3>
+                        <h2>TeamA points:{points.teamA}, sets:{sets.teamA}</h2>
+                        <h2>TeamB points:{points.teamB}, sets:{sets.teamB}</h2>
                     </div>
-                    <button onClick={handlePoints} id="teamA">Point A</button>
-                    <button onClick={handlePoints} id="teamB">Point B</button>
+                    <div className="stat-box">
+                        <h3>Stats</h3>
+                        <div className="stat-teamA">
+                            <h4>TeamA</h4>
+                            <p>Attack: {teamA.attack}</p>
+                            <p>Block: {teamA.block}</p>
+                            <p>Serve: {teamA.serve}</p>
+                            <p>Opponent error: {teamA.error}</p>
+                        </div>
+                        <div className="stat-teamB">
+                            <h4>TeamB</h4>
+                            <p>Attack: {teamB.attack}</p>
+                            <p>Block: {teamB.block}</p>
+                            <p>Serve: {teamB.serve}</p>
+                            <p>Opponent error: {teamB.error}</p>
+                        </div>
+                    </div>
+
                 </section>
             </>
         )
